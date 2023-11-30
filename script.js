191d27bb2fdd54f7ad904391d9e25c6b048f219b
n <106566141+fogmogreal@users.noreply.github.com>
2023-11-30 11:42:52 -0300

    Commit Init

---
+++ script.js
@@ -0,0 +1,13 @@
+//---------------------------- Script para funcionamento da barra de navegação do menu mobile
+
+const menumobile = document.querySelector(".menumobile");
+const nav = document.querySelector(".nav");
+const zap = document.querySelector(".zap");
+
+menumobile.addEventListener("click", () => nav.classList.toggle("active"));
+menumobile.addEventListener("click", () => zap.setAttribute.style("display: none"));
+
+window.addEventListener("load", function() {
+    var loader = document.getElementById("preloader");
+    loader.style.display = "none";
+});
